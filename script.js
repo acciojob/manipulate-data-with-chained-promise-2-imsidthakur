cd5caf8ad89e547b17c943ef07b00d88da077b99
imsidthakur <106941477+imsidthakur@users.noreply.github.com>
2024-11-28 10:40:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+
+const output = document.getElementById("output");
+
+
+const promise = new Promise((res,rej)=> {
+		res([1, 2, 3, 4]);
+})
+
+promise.then((res) =>{
+  const filtered = res.filter((num) => num%2 == 0);
+  let str = "";
+  for(let i = 0;i<filtered.length;i++)
+  {
+    if(i != 0) str += ", "
+    str+= filtered[i];
+  }
+  setTimeout(function() {
+    output.textContent = str;
+    
+  }, 1000);
+	return filtered; 
+}).then((res)=> {
+  let str = "";
+  for(let i = 0;i<res.length;i++)
+  {
+    if(i != 0) str += ", "
+    str+= res[i]*2;
+  }
+  setTimeout(function() {
+    output.textContent = str;
+    
+  }, 2000);
+
+})
